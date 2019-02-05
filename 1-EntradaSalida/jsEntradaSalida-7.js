79bd3661336c2324f10214486d6a862ae5cbd81a
ROSALES CRISTIAN <crirosal@tmoviles.com.ar>
2019-02-05 14:39:04 -0300

    Ejercicio 7 resuelto

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -2,23 +2,44 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
+
+
 function sumar()
 {	
-	
+
+	var numero1 = parseInt(numeroUno.value);
+	var numero2 = parseInt(numeroDos.value);
+	var resultado = numero1 + numero2;
+	alert("La suma es "+resultado);
 }
 
 function restar()
-{
+{	
+
+	var numero1 = parseInt(numeroUno.value);
+	var numero2 = parseInt(numeroDos.value);
+	var resultado = numero1 - numero2;
+	alert("La resta es "+resultado);
 	
 }
 
 function multiplicar()
-{ 
+{ 	
+
+	var numero1 = parseInt(numeroUno.value);
+	var numero2 = parseInt(numeroDos.value);
+	var resultado = numero1 * numero2;
+	alert("El producto es "+resultado);
 	
 }
 
 function dividir()
-{
+{	
+
+	var numero1 = parseInt(numeroUno.value);
+	var numero2 = parseInt(numeroDos.value);
+	var resultado = numero1 / numero2;
+	alert("El cociente es "+resultado);
 	
 }
 
