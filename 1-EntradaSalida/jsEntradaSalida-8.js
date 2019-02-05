f4ed03b511bcdc9aec9f9ea4520dd3a96b18b603
ROSALES CRISTIAN <crirosal@tmoviles.com.ar>
2019-02-05 15:08:36 -0300

    Ejercicio 8 y 9 resueltos

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -2,7 +2,14 @@
 transformarlos a enteros (parseInt),realizar la operación correcta y 
 mostrar el resto entre el dividendo y el divisor.
 ej.: "El resto es 0 ."*/
+
 function SacarResto()
 {
-	
+	var dividendo = parseInt(numeroDividendo.value);
+	var divisor = parseInt(numeroDivisor.value);
+	//var cociente = dividendo / divisor; --> J.I.C.
+	var resto = divisor % dividendo;
+	//alert("El cociente es "+cociente); --> tratando de inventar algo, no funciono jajaja
+	alert("El resto es "+resto); // Esto esta mal. 50/5=10 con resto cero. y me da resto 5.
+
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -2,8 +2,20 @@
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
+
+
 function mostrarAumento()
 {
 
+	var remuneracion = parseInt(sueldo.value); 
+	var neto = remuneracion*1.1 // aumento la variable remuneracion (id sueldo) en un 10% (*1.1)
+	//var divisor = parseInt(numeroDivisor.value);
+	//resultado
+	//var cociente = dividendo / divisor; --> J.I.C.
+	//var resto = divisor % dividendo;
+	//alert("El cociente es "+cociente); --> tratando de inventar algo, no funciono jajaja
+	document.getElementById(resultado.value=neto); // asocio el valor de la variable neto al id resultado
+	//alert(resultado); // Pruebo mostrar la variable con el resultado correcto aumentado -- No funciona xq tengo un id y una variable con el mismo nombre
+	//alert(neto);//	muestra el aumento ok.
 	
 }
