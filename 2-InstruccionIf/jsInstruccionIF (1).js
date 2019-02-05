0b0862e807d7183f451afbb2266f4b61b461c98a
ROSALES CRISTIAN <crirosal@tmoviles.com.ar>
2019-02-05 15:56:27 -0300

    Ejercicio IF 1 completo

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,17 @@
-function mostrar()
+//id = edad
+
+
+
+
+function mostrar()//(if (parseInt(edad.value)=15;alert("niña bonita")) {})
 {
 //tomo la edad  
+	//var age = parseInt(edad.value)
+	var age = edad.value
 
-
+		if (age == 15) { // Comparacion es con 2 = [==] / 1 solo = es para asignar!!!
+				alert("Niña bonita");
+		}
+		//else	alert("no bonita");
 
 }//FIN DE LA FUNCIÓN
