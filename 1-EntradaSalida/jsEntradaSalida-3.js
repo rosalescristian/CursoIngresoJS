cb70a7ded1fb5c2a315b2b2c069f002df42b9710
alumno <alumno>
2019-02-04 22:14:45 -0300

    Ejercicio 3 borrador incompleto

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,9 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	//buscar id en html y copiar aca como variable y tomar el valor con .value
+	elNombre.value="crispy";
+	//var nombre
+
+	//alert(id);
 }
-
-
