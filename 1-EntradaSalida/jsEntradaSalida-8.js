17fa462032dc478d11abe18f972c277a0711f18d
alumno <alumno>
2019-02-06 21:03:05 -0300

    Ejercicio 9 bis

--- 1-EntradaSalida/jsEntradaSalida-8.js
+++ 1-EntradaSalida/jsEntradaSalida-8.js
@@ -12,14 +12,13 @@ function SacarResto()
 
 	dividendo = numeroDividendo.value;
 	divisor= numeroDivisor.value;
-	//var cociente = dividendo / divisor; --> J.I.C.
+	
 	
 	dividendo=parseInt(dividendo);
 	divisor=parseInt(divisor);
 	
 	resto = dividendo % divisor;
 	
-	//alert("El cociente es "+cociente); --> tratando de inventar algo, no funciono jajaja
-	alert("El resto es "+resto); // Esto esta mal. 50/5=10 con resto cero. y me da resto 5.
+	alert("El resto es "+resto);
 
 }
