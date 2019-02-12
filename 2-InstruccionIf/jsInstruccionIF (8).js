228629f338f2ee9d2a128eef10b1f8989e0863fd
alumno <alumno>
2019-02-11 21:35:01 -0300

    Ejercicio 8 finalizado

--- 2-InstruccionIf/jsInstruccionIF (8).js
+++ 2-InstruccionIf/jsInstruccionIF (8).js
@@ -2,7 +2,24 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+		var age;
+		var elEstadoCivil;
+
+		age = edad.value;
+		age = parseInt(age);
+
+		//elEstadoCivil = getElementById(estadoCivil).value;
+		elEstadoCivil = estadoCivil.value;
+
+		//if (age < 18 && elEstadoCivil != "Soltero") { // Comparacion es con 2 = [==] / 1 solo = es para asignar!!!
+		//
+		if (age > 17)
+		{
+			if (elEstadoCivil == "Soltero")
+			{
+				alert("Es soltero y no es menor.");
+			}
+		}
 
 
 }//FIN DE LA FUNCIÓN
