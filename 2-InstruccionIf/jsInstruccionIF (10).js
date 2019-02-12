decfcc02da984e157e4e1bb02a12b0c0d90b6810
alumno <alumno>
2019-02-11 22:15:12 -0300

    Ejercicio IF 10 - Completo

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,27 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numeroRandom;
+	numeroRandom = Math.floor((Math.random() * 10) + 1);
+	numeroRandom = parseInt(numeroRandom)
+
+alert("El resultado del examen es: "+numeroRandom);
+
+	if(numeroRandom < 5)
+	{
+		alert("Vamos, la proxima se puede");
+	}
+	else
+	{
+		if (numeroRandom>8)
+		{
+			alert("EXCELENTE");
+		}
+		else
+		{
+			alert("APROBO");
+		}
+	}
 	
 
 }//FIN DE LA FUNCIÓN
