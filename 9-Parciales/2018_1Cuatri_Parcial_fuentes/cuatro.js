a9726a9cb423c5b9748e6d49c0ecd5dfc25858fd
alumno <alumno>
2019-02-13 19:02:03 -0300

    Ejercicio Parcial 4 en proceso

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
 
+	var numeroUno;
+	var numeroDos;
+	var suma;
+
+	numeroUno = parseInt(numeroUno);
+	numeroDos = parseInt(numeroDos);
+	suma = parseInt(suma);
+
+	numeroUno = prompt("Ingrese un número.");
+	numeroDos = prompt("Ingrese otro número.");
+	suma = numeroUno + numeroDos;
+
+	if (numeroUno == numeroDos)
+	{
+		alert(numeroUno+numeroDos);
+	}	
+	if (numeroUno > numeroDos)
+	{	
+		if (suma > 10)
+			{
+				alert("La suma es "+suma+" y supero el 10.");
+			}
+	}
+
 }
