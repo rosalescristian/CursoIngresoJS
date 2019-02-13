7e6b26fba4d05b87dc69a7982d4618441a6ac2a4
ROSALES CRISTIAN <crirosal@tmoviles.com.ar>
2019-02-13 09:58:27 -0300

    Ejercicio 1 Parcial Listo

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,21 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho = parseInt(ancho);
+	largo = parseInt(largo);
+	perimetro = parseInt(perimetro);
+
+	ancho = prompt("Ingrese el ancho del rectángulo.");
+	largo = prompt("Ingrese el largo del rectángulo.");
+
+	perimetro = (2*ancho+2*largo);
+
+	alert("El perímetro del rectángulo es: "+perimetro);
+	//alert(ancho);
+	//alert(largo);
 
 }
