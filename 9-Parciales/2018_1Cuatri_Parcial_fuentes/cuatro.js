7304b7865cb09c5da5d334dbf7cdd33acc015ca2
alumno <alumno>
2019-02-13 21:11:28 -0300

    Ejercicio Parcial 4 en curso

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,28 +1,27 @@
+/* al realizar una compra, si la compra de mas de dos productos 
+se realizo un descuento del 10% y si suera los $2000 se agrega
+ un descuento adicional del 15%, si el pago es con tarjeta y no 
+ efectivo al precio final se le agrega un 10% de recargo
+*/
+
 function mostrar()
 {
 
-	var numeroUno;
-	var numeroDos;
-	var suma;
 
-	numeroUno = parseInt(numeroUno);
-	numeroDos = parseInt(numeroDos);
-	suma = parseInt(suma);
+	
 
-	numeroUno = prompt("Ingrese un número.");
-	numeroDos = prompt("Ingrese otro número.");
-	suma = numeroUno + numeroDos;
-
-	if (numeroUno == numeroDos)
-	{
-		alert(numeroUno+numeroDos);
-	}	
-	if (numeroUno > numeroDos)
-	{	
-		if (suma > 10)
-			{
-				alert("La suma es "+suma+" y supero el 10.");
-			}
-	}
 
 }
+
+/* al realizar una compra, si la compra de mas de dos productos 
+se realizo un descuento del 10% y si suera los $2000 se agrega
+ un descuento adicional del 15%, si el pago es con tarjeta y no 
+ efectivo al precio final se le agrega un 10% de recargo
+*/
+
+
+
+
+
+
+
