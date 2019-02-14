4b887ce4a031c508280f41b0c6d73e9de6adfa52
alumno <alumno>
2019-02-13 21:47:22 -0300

    Ejercicio Parcial 4 en curso (cerca)

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -7,12 +7,44 @@ se realizo un descuento del 10% y si suera los $2000 se agrega
 function mostrar()
 {
 
+	var importe;
+	var cantidad;
+	var formaPago;
+	var importeFinal;
 
-	
+	importe = prompt("Ingrese el importe.");
+	importe = parseInt(importe);
 
+	cantidad = prompt("Ingrese la cantidad.");
+	cantidad = parseInt(cantidad);	
 
+	formaPago = prompt("Ingrese Forma de Pago: 1- Efectivo / 2- TC");
+
+	if (cantidad > 2)
+	{
+		if (importe > 2000)
+			{
+				if (formaPago == 1)
+				{
+					importeFinal=importe*0.75;
+				
+				}
+					importeFinal=importe*0.85;
+			}
+			if (formaPago == 1)
+			{
+				importeFinal=importe*0.9;
+			}
+				importeFinal=importe
+	}	
+		if (formaPago == 1)
+		{
+			importeFinal=importe*0.9;
+		}
+			importeFinal=importe
+
+	alert("El valor de la compra es: "+importeFinal);
 }
-
 /* al realizar una compra, si la compra de mas de dos productos 
 se realizo un descuento del 10% y si suera los $2000 se agrega
  un descuento adicional del 15%, si el pago es con tarjeta y no 
