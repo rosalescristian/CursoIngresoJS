b4879604a460c1be42e1c1dd77e05570373f99a4
alumno <alumno>
2019-02-18 18:59:12 -0300

    Ejercicio TP Adivina el nro 1 resuelto (Sin el contador funcinando

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -7,20 +7,34 @@ secreto del 1 al 100, en la pantalla del juego
 “Usted es un ganador!!! y en solo X intentos”.
 de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
 */
-var numeroSecreto; 
+var numeroSecreto;
 var contadorIntentos;
+contadorIntentos = 0;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+	numeroSecreto = Math.floor((Math.random() * 100) + 1);
+	numeroSecreto = parseInt(numeroSecreto);
+	//var numeroSecreto;
+	console.log(numeroSecreto);
 
 }
 
 function verificar()
 {
+
+	contadorIntentos += 1;
+//	contadorIntentos = intentos.value;
+	numeroUsuario = numero.value;
+	numeroUsuario = parseInt(numeroUsuario);
+
+
+	if (numeroUsuario == numeroSecreto)
+	{
+			alert("Usted es un ganador!!! y en solo "+contadorIntentos+" intentos");
+			contadorIntentos = 0;		
+	}
 	
 	
 }
