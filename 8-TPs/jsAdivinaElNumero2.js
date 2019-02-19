f3b07b84479442f602c4f584937723d51f2333c3
ROSALES CRISTIAN <crirosal@tmoviles.com.ar>
2019-02-19 16:42:30 -0300

    Ejercicio TP Adivine el NRO 2 en proceso - No funciona todavia

--- 8-TPs/jsAdivinaElNumero2.js
+++ 8-TPs/jsAdivinaElNumero2.js
@@ -11,18 +11,79 @@ Más de 10 intentos: “afortunado en el amor!!”.*/
 
 var numeroSecreto; 
 var contadorIntentos;
+contadorIntentos = 0;
 
 function comenzar()
 {
 	//Genero el número RANDOM entre 1 y 100
-	 
-	
+	numeroSecreto = Math.floor((Math.random() * 100) + 1);
+	numeroSecreto = parseInt(numeroSecreto);
+	//var numeroSecreto;
+	console.log(numeroSecreto);
 
 }
 
 function verificar()
 {
 	
-	
+	;
+//	contadorIntentos = intentos.value;
+	contadorIntentos += 1
+	numeroUsuario = numero.value;
+	numeroUsuario = parseInt(numeroUsuario);
 
+	if (numeroUsuario == numeroSecreto)
+	{
+		switch (contadorIntentos)
+		{
+			case 1:
+				contadorIntentos == 1;
+				mensaje = "Usted es psiquico";
+				contadorIntentos = 0;
+				break;	
+			case 2:
+				contadorIntentos == 2;
+				mensaje = "Excelente Percepcion";
+				contadorIntentos = 0;
+				break;
+			case 3:
+				contadorIntentos == 3;
+				mensaje = "Esto es suerte";
+				contadorIntentos = 0;
+				break;
+			case 4:
+				contadorIntentos == 4;
+				mensaje = "Excelente Tecnica";
+				contadorIntentos = 0;
+				break;
+			case 5:
+				contadorIntentos == 5;
+				mensaje = "Usted esta en la media";
+				contadorIntentos = 0;
+				break;
+			case 6:
+				contadorIntentos == 6;
+			case 7:
+				contadorIntentos == 7;
+			case 8:
+				contadorIntentos == 8;
+			case 9:
+				contadorIntentos == 9;
+			case 10:
+				contadorIntentos == 10;
+				mensaje = "Falta Tecnica";
+				contadorIntentos = 0;
+				break;
+			default:
+				mensaje = "Afortunado en el amor";
+				contadorIntentos = 0;
+				break;
+		}	
+	}
+	else
+	{
+		alert("El nro ingresado es erroneo.");
+	}
+
+	
 }
