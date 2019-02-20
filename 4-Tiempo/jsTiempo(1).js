d7121f55172b73f928712844be8100a25a73408f
ROSALES CRISTIAN <crirosal@tmoviles.com.ar>
2019-02-20 11:42:48 -0300

    Ejercicio de Tiempo 1 Resuelto OK

--- 4-Tiempo/jsTiempo(1).js
+++ 4-Tiempo/jsTiempo(1).js
@@ -2,7 +2,7 @@ var miIntervalo ;
 
 function inicio()
 {	
-	alert("function inicio.");
+	//alert("function inicio.");
 	miIntervalo = setInterval(SegundosEnElAire, 3000);
 	
 }//FIN DE LA FUNCIÓN inicio
