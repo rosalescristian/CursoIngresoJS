4687ce8e7e76bec94153445c6a5409a39643321d
alumno <alumno>
2019-02-25 19:14:31 -0300

    Ejercicio 9 de While empezado y arrancamos con FOR

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,14 +2,35 @@ function mostrar()
 {
 
 	var contador=0;
-	// declarar variables
-	
+	var positivo=0;
+	var negativo=1;
 	var respuesta='si';
+	var numeroUno;
+	var acumulador=0;
 
-	while(respuesta!='no')
+
+
+while(respuesta == "si")
 	{
+		numeroUno=prompt("Ingrese un nro.");
+		numeroUno = parseInt(numeroUno);
+		contador = contador + 1;
+
+		if{
+			
+		}
+
+/*		if(numeroUno>0)
+		{
+			positivo = positivo + numeroUno;
+		}else
+		{
+			negativo = negativo * numeroUno;
+		}
 		
-	
+		respuesta = prompt("¿Desea seguir?")
+*/
+
 	}
 
 
--- 6-InstruccionFor/jsIteracionesFor (1).js
+++ 6-InstruccionFor/jsIteracionesFor (1).js
@@ -1,4 +1,15 @@
 function mostrar()
 {
+	//for (CREAMOS VARIABLE;COMPARACION LOGICA;ACCION){}
+	//for ( ; ; ){}
+/*	
+	var contador;
+	contador = 0;
+	while(contador <10)
+	{
+		contador=contador+1;
+	}
+*/
+	
 
 }
