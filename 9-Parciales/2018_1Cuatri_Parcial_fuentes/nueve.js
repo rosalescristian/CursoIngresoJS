1553c4670c47b2c8731efc53ef91b18104fc49ae
ROSALES CRISTIAN <crirosal@tmoviles.com.ar>
2019-03-05 20:23:55 -0300

    Ejercicio 9 parcial

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/nueve.js
@@ -1,4 +1,85 @@
 function mostrar()
 {
 
+	var marca;
+	var peso;
+	var temperaturaAlmacenamiento;
+	var respuesta = "si";
+	var contadorMarca;
+	var pesoMaximo = 100;
+	var pesoMinimo = 1;
+	var temperaturaMinimo = -30;
+	var temperaturaMaxima = 30;
+	var contadorTemperaturasPares = 0;
+	var contadorTemperaturasImpares = 0;
+	var contadorAlmacenamientoMenosCero = 0;
+	var acumuladorPeso = 0;
+	var promediopesos;
+
+
+	isNaN()
+
+	for(contadorMarca = 0 ; respuesta == "si" ; contadorMarca++)
+	{
+		marca = prompt("Ingrese la marca del producto.");
+
+		peso = prompt("Ingrese el peso del producto.(Entre 0 y 100)");
+			while(isNaN(peso) || peso < pesoMinimo || peso > pesoMaximo)
+			{
+				peso=prompt("Ha ingresado un peso invalido. Reingrese un peso entre 0 y 100");
+			}
+		peso=parseInt(peso);
+		acumuladorPeso = acumuladorPeso + peso;
+
+		temperaturaAlmacenamiento = prompt("Ingrese la temperatura de almacenamiento.");
+			while(isNaN(temperaturaAlmacenamiento) || temperaturaAlmacenamiento < temperaturaMinimo || temperaturaAlmacenamiento > temperaturaMaxima)
+			{
+				temperaturaAlmacenamiento = prompt("La temperatura es invalida, por favor reingrese.");
+			}
+		temperaturaAlmacenamiento = parseInt(temperaturaAlmacenamiento);
+
+		if(temperaturaAlmacenamiento%2==0)
+		{
+			contadorTemperaturasPares++;
+		}
+		else
+		{
+			contadorTemperaturasImpares++;
+		}
+
+		if(contadorMarca == 0)
+		{
+			pesoMaximo = peso;
+			marcaPesoMax = marca;
+			pesoMinimo = peso;
+			marcaPesoMin = marca;
+		}
+		else
+		{
+			if(peso>pesoMaximo)
+			{
+				pesoMaximo = peso;
+				marcaPesoMax = marca;
+			}
+			else
+			{
+				pesoMinimo = peso;
+				marcaPesoMin = marca;
+			}
+		}
+
+		if(temperaturaAlmacenamiento<0)
+		{
+			contadorAlmacenamientoMenosCero++;
+		}
+
+		respuesta = prompt("¿Desea seguir?");
+
+	}
+
+	promediopesos = acumuladorPeso/contadorMarca;
+
+	//alert(contadorTemperaturasPares+" "+marcaPesoMax+" "+contadorAlmacenamientoMenosCero+" "+promediopesos+" "+pesoMaximo+" "+pesoMinimo);
+	alert("Nada");
+
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/ocho.js
@@ -1,4 +1,107 @@
 function mostrar()
 {
 
+	var letra;
+	var numero;
+	var respuesta;
+	var limiteInferior = -100;
+	var limiteSuperior = 100;
+	var contadorNumeros;
+	var contadorNrosPares = 0;
+	var contadorNrosImpares = 0;
+	var contadorCeros = 0;
+	var promedio;
+	var acumuladorNumeros = 0;
+	var acumuladorNrosPositivos = 0;
+	var acumuladorNrosNegativos = 0;
+	var contadorNrosPositivos = 0;
+	var contadorNrosNegativos = 0;
+	var numeroMax;
+	var	letraMax;
+	var numeroMin;
+	var letraMin;
+	var promedioPositivos;
+
+	isNaN();
+
+	respuesta = "si";
+	acumuladorNumeros = 0;
+
+	for(contadorNumeros=1;respuesta == "si";contadorNumeros++)
+	{
+		letra = prompt("Ingrese una letra.");
+			while(!isNaN(letra))
+			{
+				letra = prompt("Ha ingresado un caracter no valido, por favor reingrese una letra.");
+			}
+	
+		numero = prompt("Ingrese un numero entre -100 y 100.");
+			while(isNaN(numero) || numero < limiteInferior || numero > limiteSuperior)
+			{
+				numero = prompt("Ha ingresado un nro erroneo, por favor reingrese nro entre -100 y 100.")
+			}
+		numero=parseInt(numero);
+		acumuladorNumeros = acumuladorNumeros + numero;
+		acumuladorNumeros = parseInt(acumuladorNumeros);
+
+		if(numero==0)
+		{
+			contadorCeros++;
+		}
+		else
+		{
+			if(numero % 2 == 0)
+			{
+				contadorNrosPares++;
+			}
+			else
+			{
+				contadorNrosImpares ++;
+			}
+		}
+
+		if(numero>0)
+		{
+			acumuladorNrosPositivos = acumuladorNrosPositivos + numero;
+			acumuladorNrosPositivos=parseInt(acumuladorNrosPositivos);
+			contadorNrosPositivos++;
+		}
+		else
+		{
+			acumuladorNrosNegativos = acumuladorNrosNegativos + numero;
+			acumuladorNrosNegativos=parseInt(acumuladorNrosNegativos);
+			contadorNrosNegativos++;
+		}
+
+		if(contadorNumeros==1)
+		{
+			numeroMax = numero;
+			letraMax = letra;
+			numeroMin = numero;
+			letraMin = letra;
+		}
+		else
+		{
+			if(numero>=numeroMax)
+			{
+				numeroMax = numero;
+				letraMax = letra;
+			}
+			else
+			{
+				numeroMin = numero;
+				letraMin = letra;
+			}
+		}
+
+		respuesta = prompt("¿Desea continuar? (si o no)");
+
+	}
+	promedio = acumuladorNumeros/contadorNumeros;
+	
+	promedioPositivos = acumuladorNrosPositivos/contadorNrosPositivos;
+	//promedioPositivos = parseInt(promedioPositivos);
+
+	document.write(contadorNrosPares+"<br>"+contadorNrosImpares+"<br>"+contadorCeros+"<br>"+promedioPositivos+"<br>"+acumuladorNumeros+"<br>"+numeroMax+"<br>"+letraMax+"<br>"+numeroMin+"<br>"+letraMin);
+	//alert(promedioPositivos);
 }
